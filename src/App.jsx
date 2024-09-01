d58a13415a92af988f174877f45a2229abec95af
Aliza Khan <alizaadi2@gmail.com>
2024-09-01 22:43:50 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './components/Home/home';
 import Login from './components/Login/login';
 import Signup from './components/Signup/signup';
-import Dashboard from '@mui/icons-material/Dashboard';
+// import Dashboard from '@mui/icons-material/Dashboard';
+import { useEffect, useState } from 'react';
+import { auth } from './firebase';
+import AppDashboard from './components/Dashboard/dashboard';
 
 function App() {
+  const [UserisAuthenticated, setUserDetails] = useState("");
+ 
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUserDetails(user.displayName);
+      } else {
+        setUserDetails("")
+      }
+    })
+  }, [])
+
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/Login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
-          <Route path="/Dashboard" element={<Dashboard />}/>
-        <Route path="/" element={<Home />} />
+          <Route path="/AppDashboard" element={<AppDashboard name={UserisAuthenticated} />} />
+          <Route path="/" element={<Home name={UserisAuthenticated} />} />
         </Routes>
       </Router>
     </div>
